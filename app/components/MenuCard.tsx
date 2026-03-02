f5974ede8415f7b462d4188fde3e446a91006a64
dherrera-code <dherr929@gmail.com>
2026-03-02 15:29:49 -0800

    recreated the home page based on html project!

--- app/components/MenuCard.tsx
+++ app/components/MenuCard.tsx
@@ -1,27 +1,29 @@
 import { Button, Card } from 'flowbite-react'
 import React from 'react'
 
-const MenuCard = () => {
+interface CardInfo {
+  title: string;
+  description: string;
+  // Path: string;
+}
+
+const MenuCard = ({title, description}: CardInfo) => {
+
+
+
   return (
-    <div>
-        <Card className="max-w-sm">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Noteworthy technology acquisitions 2021
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-      </p>
-      <Button>
-        Read more
-        <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Button>
-    </Card>
+    <div >
+      <Card className="max-w-[324px] h-auto card-color rounded-lg shadow-md border-0">
+        <h5 className="text-2xl text-center">
+          {title}
+        </h5>
+        <p className="font-normal">
+          {description}
+        </p>
+        <Button className='mt-15'>
+          Play
+        </Button>
+      </Card>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,32 @@
 import { Button, Card } from "flowbite-react";
 import Image from "next/image";
 import { GetAskingQuestions, GetGreaterOrLess, GetHelloName, GetMadLib, GetMagic8Ball, GetOddOrEven, GetRestaurant, GetReverseAlpha, GetReverseNum, GetSum } from "./services/DataService"
+import MenuCard from "./components/MenuCard";
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#E6E9EE] font-sans dark:bg-[#E6E9EE]">
       <main className="text-black">
 
-      <div className="px-5 sm:px-20 md:px-40">
-        <h1 className="text-[36px] text-center py-8">All For One!</h1>
+        <div className=" py-8 mx-auto max-w-7xl text-center lg:py-16">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">All for One!</h1>
 
-        <p className="text-center">Here are the first 10 applications I learned to create in the CodeStack Academy! These 10 projects are all using API endpoints that I created and hosted via Azure! Please enjoy playing with all of my projects!</p>
+          <p className="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48 ">Here are the first 10 applications I learned to create in the CodeStack Academy! These 10 projects are all using API endpoints that I created and hosted via Azure! Please enjoy playing with all of my projects !</p>
 
-      </div>
+        </div>
+        <div className="flex flex-wrap gap-x-9 gap-y-6 justify-center mb-5">
+          <MenuCard title="Hello World" description="Enter your name and get back a greeting!" />
+          <MenuCard title="Adding Numbers" description="Enter two numbers and get back the sum of the two numbers!" />
+          <MenuCard title="Asking Questions" description="Enter your name and time you woke up today!" />
+          <MenuCard title="Odd or Even" description="Enter a number and see if it’s odd or even! " />
+          <MenuCard title="Reverse It (alphanum)" description="Enter a string of characters and see what the reverse string looks like!" />
+          <MenuCard title="Reverse It (Numbers)" description="Enter a large number and see the digit order in reverse!" />
+          <MenuCard title="MadLib" description="Enter some basic values to generate a unique story!" />
+          <MenuCard title="Magic 8 Ball" description="Play with the magic 8 ball by asking it a question!" />
+          <MenuCard title="Greater Or Less" description="Play with the CPU to guess it’s generated number! Can you guess it on your first try?" />
+          <MenuCard title="Restaurant Picker" description="Pick a restaurant Category and it will randomly generate a restaurant you should eat at!" />
 
-        {/* <h1>Welcome to the main page starting here!</h1>
-        <p>{GetSum(3,5)}</p>
-        <p>{GetAskingQuestions("Daniel", 3)}</p>
-        <p>{GetGreaterOrLess(5,52)}</p>
-        <p>{GetHelloName("Daniel")}</p>
-        <br />
-        <p>{GetMadLib("Dan", "DMV", "Silly", "Gator", "Sad")}</p><br />
-        <p>{GetMagic8Ball("Yes?")}</p>
-        <p>{GetOddOrEven(5)}</p>
-        <p>{GetRestaurant("burger")}</p>
-        <p>{GetReverseAlpha("qwerty")}</p>
-        <p>{GetReverseNum("234561")}</p>
-        
-    <Card className="max-w-sm  dark:bg-[#FFF1E8]">
-      <h5 className="text-2xl text-[#1F2B2D]  tracking-tight ">
-        Noteworthy technology acquisitions 2021
-      </h5>
-      <p className="font-normal  dark:text-black">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-      </p>
-      <Button className="dark:bg-[#1E6FB8]">
-        Read more
-      </Button>
-    </Card> */}
-        
+        </div>
+
       </main>
     </div>
   );
