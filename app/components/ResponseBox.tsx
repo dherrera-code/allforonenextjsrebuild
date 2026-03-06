28cb4dd683c5134f7889df2d3377f5c173fc6d49
dherrera-code <dherr929@gmail.com>
2026-03-06 11:48:09 -0800

    adjust responsiveness for response box for tablet!

--- app/components/ResponseBox.tsx
+++ app/components/ResponseBox.tsx
@@ -12,9 +12,9 @@ const ResponseBox = (data: dataPassed) => {
   return (
     <div>
       <section className="flex justify-center my-9">
-        <div className=" lg:min-w-xl xl:min-w-4xl py-6 px-20 card-color rounded-lg shadow-md">
+        <div className=" lg:min-w-xl xl:min-w-4xl py-6 px-10 sm:px-20 card-color rounded-lg shadow-md">
           <h2 className="text-xl sm:text-[40px] text-center mb-6">Response!</h2>
-          <p className="sm:text-[20px] md:text-[30px] text-center">{data.response}</p>
+          <p className="sm:text-[20px] lg:text-[30px] text-center">{data.response}</p>
         </div>
       </section>
     </div>
